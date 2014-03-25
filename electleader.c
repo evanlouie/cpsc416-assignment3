63a1cf92e7af8168e10adc2df66e2ec5b99c86fc
Evan Louie <louie.evan@gmail.com>
2014-03-24 18:55:52 -0700

    bi directional somewhat working

--- electleader.c
+++ electleader.c
@@ -5,6 +5,13 @@
 //  Created by Evan Louie (m6d7) on 2014-03-21.
 //  Copyright (c) 2014 Evan Louie. All rights reserved.
 //
+/*
+    Notes to reader:
+    ================
+
+    
+
+*/
 
 #include <stdio.h>
 #include <string.h>
@@ -18,6 +25,7 @@
 #include <mpi.h>
 
 
+bool debug;
 int print_error(char *message) {
     printf("===========ERROR==========\n\t%s\n==========================\n", message);
     exit(-1);
@@ -41,9 +49,65 @@ int send_elected_message(int *message, int * world_rank, int * world_size, int *
     return *message;
 }
 
+
+
+int send_message(int *message, int *world_rank, int *world_size, int *target, int *send_count) {
+    MPI_Send(message, 1, MPI_INT, *target, 0, MPI_COMM_WORLD);
+    *send_count += 1;
+    // if (debug) printf("%d send %d to %d\n", *world_rank, *message, *target);
+
+    return 1;
+}
+
+int pass_message(int *message, int *world_rank, int *world_size, MPI_Status *status, int *send_count) {
+    int target;
+    if (status->MPI_SOURCE < *world_rank) {
+        if (*world_rank == *world_size-1) {
+            target = (*world_rank -1) % *world_size;
+        } else {
+            target = (*world_rank +1) % *world_size;
+        }
+    } else if (status->MPI_SOURCE > *world_rank) {
+        target = (*world_rank - 1) % *world_size;
+    } else {
+        print_error("INVALID: MESSAGE SENT TO SELF\n");
+    }
+    if (target < 0) target *= -1;
+
+    send_message(message, world_rank, world_size, &target, send_count);
+    return 1;
+}
+
+int send_message_both_ways(int *message, int *world_rank, int *world_size, MPI_Status *status, int *send_count) {
+    int target = (*world_rank + 1) % *world_size;
+    send_message(message, world_rank, world_size, &target, send_count);
+    target = (*world_rank + *world_size - 1) % *world_size;
+    send_message(message, world_rank, world_size, &target, send_count);
+
+    return 1;
+}
+
+int recv_message(int *message, int *last_message, int *recv_count, MPI_Status *status, int *world_rank, int *world_size, bool *recv_left, bool *recv_right) {
+    *last_message = *message;
+    MPI_Recv(message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status);
+    *recv_count += 1;
+    if (status->MPI_SOURCE < *world_rank) {
+        if (*world_rank == *world_size -1) {
+            *recv_right = true;
+        } else {
+            *recv_left = true;
+        }
+    } else {
+        *recv_right = true;
+    }
+
+    // if (debug) printf("%d recv %d from %d\n", *world_rank, *message, status->MPI_SOURCE);
+    return 1;
+}
+
 int main(int argc, char * argv[])
 {
-    bool debug = false;
+    debug = true;
     bool participant, is_leader;
     int PNUM, id, send_count, recv_count, world_size, world_rank, leader_id, message;
 
@@ -79,82 +143,86 @@ int main(int argc, char * argv[])
     leader_id = 0;
     message = 0;
     MPI_Status status;
+    bool recv_left = false;
+    bool recv_right = false;
 
     while (leader_id == 0) {
         if (world_rank != 0) {
             int last_message = message;
-            MPI_Recv(&message, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, &status);
-            recv_count++;
+            recv_message(&message, &last_message, &recv_count, &status, &world_rank, &world_size, &recv_left, &recv_right);
             // printf("Process %d received message %d from process %d\n", world_rank, message, world_rank - 1);
             
             if (message < 0) {
                 // CATCH FOR ELECTED MESSAGE
                 participant = false;
                 leader_id = message*-1;
-                send_elected_message(&message, &world_rank, &world_size, &send_count);
+                // send_elected_message(&message, &world_rank, &world_size, &send_count);
+                if (message != id) pass_message(&message, &world_rank, &world_size, &status, &send_count);
                 printf("rank=%d, id=%d, leader=%d, mrcvd=%d, msent=%d\n", world_rank, id, leader_id, recv_count, send_count);
             } else if (message > id) {
-                send_election_message(&message, &participant,&world_rank, &world_size, &send_count);
+                pass_message(&message, &world_rank, &world_size, &status, &send_count);
             } else if ((message < id) && participant == false) {
                 message = id;
-                send_election_message(&message, &participant,&world_rank, &world_size, &send_count);
+                pass_message(&message, &world_rank, &world_size, &status, &send_count);
             } else if ((message < id) && participant == true) {
                 // TODO: discard message
                 message = last_message;
-                send_election_message(&message, &participant,&world_rank, &world_size, &send_count);
-            } else if (message == id) {
+                pass_message(&message, &world_rank, &world_size, &status, &send_count);
+            } else if (recv_left && recv_right) {
+                if (message < last_message) message = last_message;
                 leader_id = message;
                 // START SECOND PART OF ALGORITHM (ELECTED MESSAGE)
                 participant = false;
                 message = message*-1;
-                send_elected_message(&message, &world_rank, &world_size, &send_count);
+                send_message_both_ways(&message, &world_rank, &world_size, &status, &send_count);
                 printf("rank=%d, id=%d, leader=%d, mrcvd=%d, msent=%d\n", world_rank, id, leader_id, recv_count, send_count);
             }
         } else {
             // Set the message's value if you are process 0
             if (election_started == false) {
                 if (message == 0) message = id;
-                send_election_message(&message, &participant,&world_rank, &world_size, &send_count);
+                send_message_both_ways(&message, &world_rank, &world_size, &status, &send_count);
                 election_started = true;
             }
             
             int last_message = message;
-            MPI_Recv(&message, 1, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD, &status);
-            recv_count++;
-            // printf("Process %d received message %d from process %d\n", world_rank, message, world_size - 1);
+            recv_message(&message, &last_message, &recv_count, &status, &world_rank, &world_size, &recv_left, &recv_right);
+            // printf("Process %d received message %d from process %d\n", world_rank, message, world_rank - 1);
+            
             if (message < 0) {
                 // CATCH FOR ELECTED MESSAGE
                 participant = false;
                 leader_id = message*-1;
-                send_elected_message(&message, &world_rank, &world_size, &send_count);
+                // send_elected_message(&message, &world_rank, &world_size, &send_count);
+                if (message != id) pass_message(&message, &world_rank, &world_size, &status, &send_count);
                 printf("rank=%d, id=%d, leader=%d, mrcvd=%d, msent=%d\n", world_rank, id, leader_id, recv_count, send_count);
             } else if (message > id) {
-                send_election_message(&message, &participant,&world_rank, &world_size, &send_count);
+                pass_message(&message, &world_rank, &world_size, &status, &send_count);
             } else if ((message < id) && participant == false) {
                 message = id;
-                send_election_message(&message, &participant,&world_rank, &world_size, &send_count);
+                pass_message(&message, &world_rank, &world_size, &status, &send_count);
             } else if ((message < id) && participant == true) {
                 // TODO: discard message
                 message = last_message;
-                send_election_message(&message, &participant,&world_rank, &world_size, &send_count);
-            } else if (message == id) {
+                pass_message(&message, &world_rank, &world_size, &status, &send_count);
+            } else if (recv_left && recv_right) {
+                if (message < last_message) message = last_message;
                 leader_id = message;
                 // START SECOND PART OF ALGORITHM (ELECTED MESSAGE)
                 participant = false;
                 message = message*-1;
-                send_elected_message(&message, &world_rank, &world_size, &send_count);
+                send_message_both_ways(&message, &world_rank, &world_size, &status, &send_count);
                 printf("rank=%d, id=%d, leader=%d, mrcvd=%d, msent=%d\n", world_rank, id, leader_id, recv_count, send_count);
             }
         }
     }
-
     int total_send_count = send_count;
     int total_recv_count = recv_count*-1;
     if (leader_id == id) {
         MPI_Send(&total_send_count, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
         MPI_Send(&total_recv_count, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
         
-        for (int i = 0; i<=2; i++) {
+        for (int i = 0; i<=1; i++) {
             MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             if (message > 0) {
                 total_send_count = message;
